f546c30d9b9312b93839d3d11cf945a85dbe6242
Rakesh reddy <regatirakesh1234@gmail.com>
2026-07-31 19:21:39 +0530

    Update postgre.js

--- services/postgre.js
+++ services/postgre.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "trackaiuser",
-    host: "dpg-d9lklmp42hec739tvtjg-a.singapore-postgres.render.com",
-    database: "trackai_3msz",
-    password: "xReQd4pJiVzzBG5PNyT2TX0COkFCjMDL",
-    port: 5432,
+   connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
