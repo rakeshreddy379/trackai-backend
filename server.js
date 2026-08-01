c8ccf72a3cc39007d94027ea9fbc55cc95a2ec7f
Rakesh reddy <regatirakesh1234@gmail.com>
2026-08-01 08:13:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
 
 // ---------------- Start Server ----------------
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log(` Server running on http://localhost:${PORT}`);
