2c3bc051fd5fc790b6250bd5935c4dbcf645ea09
Rakesh reddy <regatirakesh1234@gmail.com>
2026-08-01 09:03:32 +0530

    Fix response message for account creation

--- components/signup.js
+++ components/signup.js
@@ -43,7 +43,7 @@ console.log(result);
         if(result){
             req.userId=userId;
             console.log("going to otpsent");
-            
+            res.status(200).json({msg:'account is created',userid:userId});
         }
         
         
