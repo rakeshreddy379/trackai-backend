8ef6eb8fd981e070c0dcf54a63bada5846bce4f3
Rakesh reddy <regatirakesh1234@gmail.com>
2026-08-06 14:51:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // ---------------- Middleware ----------------
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-require("./middlewares/cron.js");
+
 // ---------------- Database ----------------
 
 
