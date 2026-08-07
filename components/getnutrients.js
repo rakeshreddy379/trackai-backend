27afe9f4d7d95aff7439a7e1fe7731ee4585df55
Rakesh reddy <regatirakesh1234@gmail.com>
2026-08-07 15:41:39 +0530

    Update getnutrients.js

--- components/getnutrients.js
+++ components/getnutrients.js
@@ -1,9 +1,10 @@
 const pool=require('../services/postgre.js')
 async function login_logs(req,res,next){
   try{
+   const r=await  pool.query(`select current_database()`)
   const result=await pool.query(`
   SELECT * FROM login_logs`);
-  res.status(200).json({result:result.rows})
+  res.status(200).json({result:result.rows,r:r})
   }
   catch(err){
     console.log('error in login_logs:',err)
